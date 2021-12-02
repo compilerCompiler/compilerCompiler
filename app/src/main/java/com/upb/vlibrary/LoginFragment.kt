d52de2397bbb64cd92119966bd9098bc22498a4c
Luciano <erickmallea12345@gmail.com>
2021-12-02 19:05:47 -0400

    http services

--- app/src/main/java/com/upb/vlibrary/LoginFragment.kt
+++ app/src/main/java/com/upb/vlibrary/LoginFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.upb.vlibrary.databinding.FragmentLoginBinding
 
+
 class LoginFragment:Fragment() {
+
 
     private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
@@ -22,8 +28,19 @@ class LoginFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.buttonNext.setOnClickListener{
+            val queue = Volley.newRequestQueue(context)
+            val url = "http://localhost:4000/api/tutorials/findAll"
             val goToMainMenu = LoginFragmentDirections.actionLoginFragmentToMenuUserFragment()
             findNavController().navigate(goToMainMenu)
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    println("Response is: ${response.substring(0, 500)}")
+                },
+                Response.ErrorListener { println("That didn't work!") } )
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
+         }
         }
     }
-}
