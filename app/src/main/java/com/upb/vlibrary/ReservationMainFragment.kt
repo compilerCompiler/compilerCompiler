5b2cfb1d311f33066e31a228454e607b7b38e285
Manu200162 <manli200134@gmail.com>
2021-12-05 21:15:48 -0400

    Reservation main page almost completed

--- app/src/main/java/com/upb/vlibrary/ReservationMainFragment.kt
+++ app/src/main/java/com/upb/vlibrary/ReservationMainFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.upb.vlibrary.databinding.FragmentReservationMainBinding
 
 class ReservationMainFragment: Fragment() {
 
+    private val articleToReserveAdapter = SearchArticlesToReserveAdapter()
     private lateinit var binding: FragmentReservationMainBinding
+    private val articleSearchViewModel: ArticleSearchViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +26,20 @@ class ReservationMainFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        articleSearchViewModel.updateListBooks()
+        binding.rvReservationList.adapter=articleToReserveAdapter
+        val layoutManager= LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        binding.rvReservationList.layoutManager=layoutManager
+        LinearSnapHelper().attachToRecyclerView(binding.rvReservationList)
+
+        articleToReserveAdapter.setOnLibroClickListener {
+            val goToReservationForm= ReservationMainFragmentDirections.actionReservationMainFragmentToReservationFormFragment(it)
+            findNavController().navigate(goToReservationForm)
+        }
+
+        articleSearchViewModel.libros.observe(viewLifecycleOwner){
+            articleToReserveAdapter.addAll(it)
+        }
 
     }
 }
---
+++ app/src/main/java/com/upb/vlibrary/SearchArticlesToReserveAdapter.kt
@@ -0,0 +1,49 @@
+package com.upb.vlibrary
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.upb.vlibrary.databinding.ArticleItemBinding
+import com.upb.vlibrary.databinding.FragmentArticleSearchInfoBinding
+import com.upb.vlibrary.databinding.ReservationItemBinding
+
+class SearchArticlesToReserveAdapter:RecyclerView.Adapter<SearchArticlesToReserveListViewHolder>(){
+    private val librosList: MutableList<Books> = mutableListOf()
+    private var onLibroItemClickListener:((libros:Books)-> Unit)? =null
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addAll(newElementList: List<Books>) {
+        librosList.clear()
+        librosList.addAll(newElementList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchArticlesToReserveListViewHolder {
+        val binding= ReservationItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SearchArticlesToReserveListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchArticlesToReserveListViewHolder, position: Int) {
+        holder.bind(librosList[position])
+        holder.binding.root.setOnClickListener{
+            onLibroItemClickListener?.invoke(librosList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return librosList.size
+    }
+
+    fun setOnLibroClickListener(onLibroItemClickListener:((libros:Books)->Unit)?){
+        this.onLibroItemClickListener=onLibroItemClickListener
+    }
+
+}
+
+class SearchArticlesToReserveListViewHolder(val binding: ReservationItemBinding): RecyclerView.ViewHolder(binding.root){
+    fun bind(libros:Books){
+        binding.libroToReserve=libros
+    }
+}
+
